100219b0614ec7e40c75ae470fc6983d7dcbfa6e
Anton Korzunov <thekashey@gmail.com>
2018-04-21 19:55:28 +1000

    d.ts

---
+++ react-map-chart.d.ts
@@ -0,0 +1,15 @@
+declare module 'react-map-chart' {
+    import * as React from 'react';
+
+    interface Props {
+        data: { [key: string]: number };
+
+        className?: string,
+
+        native?: boolean,
+
+        styler: (value: number, code: string) => object
+    }
+
+    export default class ReactFocusLock extends React.Component<Props> {}
+}
